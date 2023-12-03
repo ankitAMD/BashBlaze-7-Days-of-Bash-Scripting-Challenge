2a5357bd824ac3d496589d0270e568d523d3df0b
Ankit Gupta <gankit974@gmail.com>
2023-12-03 16:34:35 +0530

    Update Ankit_Explorer1.sh

--- Challenges/Day_2/Ankit_Explorer1.sh
+++ Challenges/Day_2/Ankit_Explorer1.sh
@@ -4,12 +4,12 @@
 echo "Welcome to the File and Directory Size Listing Script!"
 echo "-----------------------------------------------------"
 
-# List all files and directories in the current path
+# List all files and directories in the current path and store in File10.txt
 echo "Files and directories in the current path:"
 ls -lh > file10.txt
 
+#remove 1st row of file10.txt and then store in  file11.txt
 tail -n +2 file10.txt > file11.txt
 
 # List sizes for each file and directory
-
 awk '{print $9 " - " "("$5")"}' file11.txt
